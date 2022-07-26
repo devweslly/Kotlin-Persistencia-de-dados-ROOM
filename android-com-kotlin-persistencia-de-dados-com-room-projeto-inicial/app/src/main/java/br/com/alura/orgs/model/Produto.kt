6829f5be2fff1e5e31c5dc24dd53da62877b08a7
Weslly Viana <devweslly@gmail.com>
2022-07-26 10:15:41 -0300

    Implementando Entidade - @PrimaryKey

--- android-com-kotlin-persistencia-de-dados-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/model/Produto.kt
+++ android-com-kotlin-persistencia-de-dados-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/model/Produto.kt
@@ -2,12 +2,18 @@ package br.com.alura.orgs.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
-@Entity
+@Entity // sempre necessário, independente da Entidade
 @Parcelize
 data class Produto(
+        // A notação @PrimaryKey (chave-primária) sempre necessária, determina um campo
+        // ou coluna que terá um valor único para cada registro, neste caso,
+        // valor único para a propetie id.
+        // O id sempre precisa passar o valor 0, para o Room entender que ele vai gerar este valor.
+        @PrimaryKey(autoGenerate = true) val id: Long = 0L,
         val nome: String,
         val descricao: String,
         val valor: BigDecimal,
