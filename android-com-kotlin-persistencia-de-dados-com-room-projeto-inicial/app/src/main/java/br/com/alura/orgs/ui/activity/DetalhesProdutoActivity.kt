ad7eb07ec13fc83d7d5e36cdf20f1752a4c15b0c
Weslly Viana <devweslly@gmail.com>
2022-07-29 13:22:30 -0300

    Implementando configuração de listener dos menus

--- android-com-kotlin-persistencia-de-dados-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ android-com-kotlin-persistencia-de-dados-com-room-projeto-inicial/app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -2,12 +2,16 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import br.com.alura.orgs.R
 import br.com.alura.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.alura.orgs.extensions.formataParaMoedaBrasileira
 import br.com.alura.orgs.extensions.tentaCarregarImagem
 import br.com.alura.orgs.model.Produto
+
+private const val TAG = "DetalhesProduto"
 
 class DetalhesProdutoActivity : AppCompatActivity() {
 
@@ -28,6 +32,28 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         // que é a referencia que recebe via parametro no onCreateOptionsMenu
         menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    // Método para sobrescrita para configuração de Listener
+    // Filtra qualquer tipo de menuque foi selecionado pela Activity
+    // Se algum menu for acionado, este método é acionado também
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Identificando qual menu foi selecionado (para depois fazer uma ação)
+        // O parametro item que informa (filtra) qual menu foi selecionado. Assim configuramos listener para cada menu
+        when(item.itemId){
+            // Filtrando opções que podem acontecer
+            // Opção 1
+            R.id.menu_detalhes_produto_remover -> {
+                // O que fazer (ação) ao selecionar opção 1
+                Log.i(TAG, "onOptionsItemSelected: remover")
+            }
+            // Opção 1
+            R.id.menu_detalhes_produto_editar -> {
+                // O que fazer (ação) ao selecionar opção 2
+                Log.i(TAG, "onOptionsItemSelected: editar")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun tentaCarregarProduto() {
